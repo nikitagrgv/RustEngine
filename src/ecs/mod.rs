6cd4cebfa223a7bd4b0376983864bf6730cacdf0
Nikita Grigorev <pettyaelse@gmail.com>
2023-09-14 00:45:16 +0700

    renew

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -8,9 +8,4 @@ use std::ops::{Deref, DerefMut};
 #[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
 pub struct Entity(usize);
 
-/// System says about set of components that will be tracked by Ecs.
-/// You can get a set of entities with these components using the SystemId
-#[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
-pub struct SystemId(usize);
-
 
