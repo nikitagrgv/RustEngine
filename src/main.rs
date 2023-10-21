11ed4c48b1824beeee29263ae58d7ebee5f5b93e
Nikita Grigorev <pettyaelse@gmail.com>
2023-10-22 02:28:17 +0700

    w

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ mod input;
 mod utils;
 
 use crate::ecs::*;
-use crate::engine::*;
 use crate::engine::logic::*;
+use crate::engine::*;
 
 use crate::input::Input;
 use glm::Vec3;
@@ -23,36 +23,53 @@ struct Mass(f32);
 #[derive(Clone, Copy, Debug)]
 struct Velocity(Vec3);
 
-fn init_example(state: &mut i32, ei: &mut EngineInterface) {
+struct ExampleState {
+    var: f32,
+}
+
+fn init_example(state: &mut ExampleState, ei: &mut EngineInterface) {
     println!("inited!");
 }
 
-fn update_example(state: &mut i32, ei: &mut EngineInterface) {
+fn update_example(state: &mut ExampleState, ei: &mut EngineInterface) {
     let input = ei.get_subsystem::<Input>();
-    if input.is_key_pressed(Scancode::W) {
-        println!("PRESSED!");
+
+    if input.is_key_down(Scancode::Left) {
+        state.var -= 0.01f32;
     }
-    if input.is_key_released(Scancode::W) {
-        println!("RELEASED!");
+
+    if input.is_key_down(Scancode::Right) {
+        state.var += 0.01f32;
     }
-    if input.is_key_down(Scancode::A) {
-        println!("DOWN!");
+
+    if state.var > 1f32 || state.var < 0f32
+    {
+        state.var = 0f32;
     }
+
+    unsafe { gl::ClearColor(state.var, 0f32, 0f32, 1.0) };
 }
 
-fn post_update_example(state: &mut i32, ei: &mut EngineInterface) {}
+fn post_update_example(state: &mut ExampleState, ei: &mut EngineInterface) {
 
-fn shutdown_example(state: &mut i32, ei: &mut EngineInterface) {
+}
+
+fn render_example(state: &mut ExampleState, ei: &mut EngineInterface) {
+
+}
+
+fn shutdown_example(state: &mut ExampleState, ei: &mut EngineInterface) {
     println!("shutdown!");
 }
 
 fn main() {
     let mut engine = Engine::new();
 
-    let mut logic = StateLogic::<i32>::new(123);
+    let mut logic = StateLogic::new(ExampleState { var: 0f32 });
     logic.add_function(init_example, LogicFuncType::Init);
     logic.add_function(update_example, LogicFuncType::Update);
     logic.add_function(post_update_example, LogicFuncType::PostUpdate);
+    logic.add_function(render_example, LogicFuncType::Render);
     logic.add_function(shutdown_example, LogicFuncType::Shutdown);
     engine.add_logic(logic);
 
